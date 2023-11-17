333f7257cd6b6bbf7105b3d5c86b3ebd1d51bc6f
Daniel <51673798+danierumr@users.noreply.github.com>
2023-11-17 09:31:00 -0300

    Deletando srquivos pac-man (Gabriel)

--- Source/Actors/Actor.h
+++ Source/Actors/Actor.h
@@ -8,7 +8,7 @@
 
 #pragma once
 #include <vector>
-#include <SDL2/SDL_stdinc.h>
+#include <SDL_stdinc.h>
 #include "../Math.h"
 #include "../Components/ColliderComponents/AABBColliderComponent.h"
 
@@ -22,7 +22,7 @@ enum class ActorState
 class Actor
 {
 public:
-    Actor(class Game* game);
+    explicit Actor(class Game* game);
     virtual ~Actor();
 
     // Update function called from Game (not overridable)
